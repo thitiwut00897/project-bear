100b5649a5e396e5da553936f1cd1dfaca1ecd8f
Kittipot <Kittipot321@gmail.com>
2020-04-02 09:43:50 +0700

    register front-end

--- projectbear/main/views.py
+++ projectbear/main/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 # Create your views here.
+@login_required
+def index(request):
+    return HttpResponse('Index มาแล้ว')
+def my_login(request):
+    return render(request,'login_page.html')
+def my_register(request):
+    return render(request,'register_page.html')
--- projectbear/projectbear/settings.py
+++ projectbear/projectbear/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'main',
+    'manage',
+    'report',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +57,7 @@ ROOT_URLCONF = 'projectbear.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,3 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+LOGIN_URL = '/login/'
